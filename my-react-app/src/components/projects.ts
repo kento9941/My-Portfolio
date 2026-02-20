766faf4ef56bf5e73ec3cab8c59b4f584bc70554
kento9941 <kento9941@gmail.com>
2026-02-20 20:25:29 +1030

    feat: added strudel music sequencer detail page

--- my-react-app/src/components/projects.ts
+++ my-react-app/src/components/projects.ts
@@ -9,8 +9,12 @@ export const projects = [
         roles: "Design / Frontend / Backend",
         summary: "A browser-based music sequencer built to explore the intersection of web technology and music production. Leveraging Strudel.js, React.js and Zustand, I developed a system that supports multi-instrument tracks with precise, per-note control over pitch and volume.",
         image1: "/sms.gif",
-        image2: "/sms.gif",
-        image3: "/sms.gif",
-        image4: "/sms.gif",
+        image2: "/sms2.png",
+        image3: "/sms3.gif",
+        image4: "/sms4.png",
+        bg1: "/black-bg.jpg",
+        bg2: "/grey-bg.jpg",
+        bg3: "/black-bg.jpg",
+        bg4: "/grey-bg.jpg",
     }
 ] 
--- my-react-app/src/pages/WorkDetail.tsx
+++ my-react-app/src/pages/WorkDetail.tsx
@@ -8,25 +8,34 @@ export default function WorkDetail() {
 
     return (
         <motion.main
-            className="relative w-screen min-h-screen flex flex-col pt-[15vh] pb-[50vh] pl-[20vw] pr-[5vw]"
-            initial={{ opacity: 0, filter: "blur(3px)" }}
-            animate={{ opacity: 1, filter: "blur(0px)" }}
-            exit={{ opacity: 0, filter: "blur(0px)" }}
+            className="relative w-screen min-h-screen flex flex-col pt-[40vh] pb-[30vh] pl-[15vw] pr-[5vw]"
+            exit={{ opacity: 0, filter: "blur(3px)" }}
             transition={{ duration: 0.8, ease: "easeOut" }}
         >
-            <div className="absolute top-[30vh] left-[20vw] w-[75vw] h-[0.5px] bg-white" />
+            <motion.div
+                className="fixed top-[30vh] left-[15vw] w-[80vw] h-[0.5px] bg-white"
+                initial={{ opacity: 0, filter: "blur(3px)" }}
+                animate={{ opacity: 1, filter: "blur(0px)" }}
+                exit={{ opacity: 0, filter: "blur(3px)" }}
+                transition={{ duration: 0.8, ease: "easeOut" }}
+            />
 
-            <div className="flex flex-col">
-                <div className="h-[15vh] flex items-end text-[3rem] font-thin mb-10">
+            <motion.div
+                className="fixed top-[15vh] left-[15vw] w-[80vw] flex flex-row items-end justify-between"
+                initial={{ opacity: 0, filter: "blur(3px)" }}
+                animate={{ opacity: 1, filter: "blur(0px)" }}
+                exit={{ opacity: 0, filter: "blur(3px)" }}
+                transition={{ duration: 0.8, ease: "easeOut" }}
+            >
+                <div className="h-[15vh] flex items-end text-[3rem] font-thin">
                     <h1>{data?.title}</h1>
                 </div>
-
-                <div className="flex flex-row gap-[3rem] text-[1rem] font-light mb-8">
+                <div className="flex flex-row items-end gap-[1rem] text-[1rem] font-light mb-2">
                     <a
                         href={data?.github}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="inline-flex items-center gap-2"
+                        className="inline-flex items-center gap-1"
                     >
                         GitHub
                         <span>
@@ -43,7 +52,7 @@ export default function WorkDetail() {
                             href={data?.demo}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="inline-flex items-center gap-2"
+                            className="inline-flex items-center gap-1"
                         >
                             Demo
                             <span>
@@ -56,8 +65,66 @@ export default function WorkDetail() {
                         </a>
                     )}
                 </div>
+            </motion.div>
 
-                <div className="flex flex-row items-start justify-between font-light mb-[30vh]">
+            <motion.div
+                className="flex flex-col"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.8, ease: "easeOut" }}
+            >
+                <div className="w-full flex flex-col items-center justify-start gap-[10vh] pb-[40vh]">
+                    <div className="relative w-[50vw] h-[60vh] flex items-center justify-center">
+                        <img
+                            src={data?.bg1}
+                            className="absolute inset-0 w-full h-full object-contain"
+                        />
+                        <img
+                            src={data?.image1}
+                            alt={data?.title}
+                            className="z-10 w-full h-full object-contain p-10"
+                        />
+                    </div>
+
+                    <div className="relative w-[50vw] h-[60vh] flex items-center justify-center">
+                        <img
+                            src={data?.bg2}
+                            className="absolute inset-0 w-full h-full object-contain"
+                        />
+                        <img
+                            src={data?.image2}
+                            alt={data?.title}
+                            className="z-10 w-full h-full object-contain p-10"
+                        />
+                    </div>
+
+                    <div className="relative w-[50vw] h-[60vh] flex items-center justify-center">
+                        <img
+                            src={data?.bg3}
+                            className="absolute inset-0 w-full h-full object-contain"
+                        />
+                        <img
+                            src={data?.image3}
+                            alt={data?.title}
+                            className="z-10 w-full h-full object-contain p-10"
+                        />
+                    </div>
+
+                    <div className="relative w-[50vw] h-[60vh] flex items-center justify-center">
+                        <img
+                            src={data?.bg4}
+                            className="absolute inset-0 w-full h-full object-contain"
+                        />
+                        <img
+                            src={data?.image4}
+                            alt={data?.title}
+                            className="z-10 w-full h-full object-contain p-10"
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-row items-start justify-between font-light">
                     <div className="w-[35vw] flex flex-col gap-8">
                         <div className="flex flex-col items-start justify-between gap-2">
                             <h2 className="text-[1.2rem]">TOOLS</h2>
@@ -86,57 +153,7 @@ export default function WorkDetail() {
                         <p className="text-[1rem]">{data?.summary}</p>
                     </div>
                 </div>
-
-                <div className="w-full flex flex-col items-center justify-start gap-[10vh]">
-                    <div className="relative w-[60vw] h-[80vh] flex items-center justify-center">
-                        <img
-                            src="/black-bg.jpg"
-                            className="absolute inset-0 w-full h-full object-contain"
-                        />
-                        <img
-                            src={data?.image1}
-                            alt={data?.title}
-                            className="z-10 w-full h-full object-contain p-10"
-                        />
-                    </div>
-
-                    <div className="relative w-[60vw] h-[80vh] flex items-center justify-center">
-                        <img
-                            src="/grey-bg.jpg"
-                            className="absolute inset-0 w-full h-full object-contain"
-                        />
-                        <img
-                            src={data?.image2}
-                            alt={data?.title}
-                            className="z-10 w-full h-full object-contain p-10"
-                        />
-                    </div>
-
-                    <div className="relative w-[60vw] h-[80vh] flex items-center justify-center">
-                        <img
-                            src="/colorful-bg.png"
-                            className="absolute inset-0 w-full h-full object-contain"
-                        />
-                        <img
-                            src={data?.image3}
-                            alt={data?.title}
-                            className="z-10 w-full h-full object-contain p-10"
-                        />
-                    </div>
-
-                    <div className="relative w-[60vw] h-[80vh] flex items-center justify-center">
-                        <img
-                            src="/white-bg.jpg"
-                            className="absolute inset-0 w-full h-full object-contain"
-                        />
-                        <img
-                            src={data?.image4}
-                            alt={data?.title}
-                            className="z-10 w-full h-full object-contain p-10"
-                        />
-                    </div>
-                </div>
-            </div>
+            </motion.div>
         </motion.main>
     )
 }
