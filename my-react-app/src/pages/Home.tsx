1f156850b539db3718551101f49cb9f93ef44fca
kento9941 <kento9941@gmail.com>
2026-03-01 16:53:28 +1030

    fix: fixed minor issue

--- my-react-app/src/pages/Home.tsx
+++ my-react-app/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
 import { useCursorStore } from "../store/useCursorStore";
 import ParallaxImage from "../components/parallax-image";
 import { NavLink } from "react-router-dom";
